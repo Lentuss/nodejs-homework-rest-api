7aac989615ec0abade059ed51635c9117e6e5fc1
Olena Pohybil <itlentus@gmail.com>
2022-10-30 13:42:41 +0200

    add avatarUpdate

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,15 +1,22 @@
 const { User } = require("../../models/user");
 const bcrypt = require("bcryptjs");
 const { RequestError } = require("../../helpers");
+const gravatar = require("gravatar");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
+  const avatarURL = gravatar.url(email);
   const user = await User.findOne({ email });
   if (user) {
     throw RequestError(409, "Email already in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ name, email, password: hashPassword });
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashPassword,
+    avatarURL,
+  });
   res.status(201).json({ name: newUser.name, email: newUser.email });
 };
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { validation, ctrlWrapper, authenticate } = require("../../middlewares");
+const {
+  validation,
+  ctrlWrapper,
+  authenticate,
+  upload,
+} = require("../../middlewares");
 const { schemas } = require("../../models/user");
 const { users } = require("../../controllers");
 
-const { updateSubscription, current, logout } = users;
+const { updateSubscription, current, logout, updateAvatar } = users;
 const { subscriptionSchema } = schemas;
 
 const validateSubscription = validation(subscriptionSchema);
@@ -14,6 +19,12 @@ router.patch(
   "/:userId/subscription",
   validateSubscription,
   ctrlWrapper(updateSubscription)
+);
+router.patch(
+  "/:userId/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrlWrapper(updateAvatar)
 );
 router.get("/current", authenticate, ctrlWrapper(current));
 router.get("/logout", authenticate, ctrlWrapper(logout));
